4bd99b131c8d442371dcbec9e90628bfff5caafa
gillockZ400 <bitrate27@gmail.com>
2016-08-21 16:59:55 +0200

    remove datastore

--- app.py
+++ app.py
@@ -1,81 +1,40 @@
 #!/usr/bin/env python3
 import connexion
-from connexion import NoContent
 import datetime
 import logging
-from gcloud import datastore
-import json
+
+from connexion import NoContent
 
 # our memory-only api storage
-def Json(api):
-    """Translates Datastore results into the format expected by the
-    application.
+APIS = {}
 
-    Datastore typically returns:
-        [Entity{key: (kind, id), prop: val, ...}]
 
-    This returns:
-        {id: id, prop: val, ...}
-    """
-    if not api:
-        return None
-    j = {'id': api['id'], 
-        'name': api['name'],
-        'api_type': api['api_type'],
-    }
-    return api
+def get_apis(limit, api_type=None):
+    return [api for api in APIS.values() if not api_type or api['api_type'] == api_type][:limit]
 
-def get_client():
-    return datastore.Client('lithe-strata-123819')
-
-def get_apis(api_type=None):
-    ds = get_client()
-    query = ds.query(kind='API', order=['name'])
-    if (api_type != None): query.where('api_type ==',api_type)
-    prepJson = []
-    apis = query.fetch()
-    for a in apis:
-        prepJson.append(Json(a))
-
-    return prepJson
 
 def get_api(api_id):
-    ds = get_client()
-    key = ds.key('API', api_id)
-    entity = ds.get(key)
-    exists = (entity != None)
-    if exists:
-        return Json(entity)
-    else: 
-        return ('Not found', 404)
+    api = APIS.get(api_id)
+    return api or ('Not found', 404)
 
 
 def put_api(api_id, api):
-    ds = get_client()
-    key = ds.key('API', api_id)
-    entity = ds.get(key)
-    exists = (entity != None)
+    exists = api_id in APIS
     api['id'] = api_id
     if exists:
         logging.info('Updating api %s..', api_id)
-        api['updated'] = datetime.datetime.utcnow()
+        APIS[api_id].update(api)
     else:
         logging.info('Creating api %s..', api_id)
-        entity = datastore.Entity(key=key)
         api['created'] = datetime.datetime.utcnow()
-
-    entity.update(Json(api))
-    ds.put(entity)   
+        APIS[api_id] = api
     return NoContent, (200 if exists else 201)
 
+
 def delete_api(api_id):
-    ds = get_client()
-    key = ds.key('API', api_id)
-    entity = ds.get(key)
-    exists = (entity != None)
-    if exists:
+    if api_id in APIS:
         logging.info('Deleting api %s..', api_id)
-        ds.delete(key)
+        del APIS[api_id]
         return NoContent, 204
     else:
         return NoContent, 404
@@ -84,7 +43,6 @@ def delete_api(api_id):
 logging.basicConfig(level=logging.INFO)
 app = connexion.App(__name__)
 app.add_api('swagger.yaml')
-
 # set the WSGI application callable to allow using uWSGI:
 # uwsgi --http :8080 -w app
 application = app.app
